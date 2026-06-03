6fdaf67d0323f365aee9d2408b40c530e8536eab
MaruliRicardoSitanggang <maruli180617@gmail.com>
2026-06-03 08:02:30 +0700

    Update PenyewaRepository.java

--- src/main/repository/PenyewaRepository.java
+++ src/main/repository/PenyewaRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PenyewaRepository extends JpaRepository<Penyewa, Long> {
-    // Mewarisi fungsi-fungsi standar CRUD JPA
 }
